d6a264b7821b5dd68746358b2ec7ec1145a9fc07
greg-hodgson <greg.hodgson1@gmail.com>
2021-10-21 00:12:35 +1100

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>{process.env.API_KEY}</p>
+        <p>{process.env.REACT_APP_API_KEY}</p>
         <p>Getting Airtable data...</p>
       </header>
     </div>
